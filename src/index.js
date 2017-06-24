824d8e510d879da2fe5a8de331d81db7fd3d76cf
anandg112 <anandg112@gmail.com>
2017-06-24 01:39:59 -0400

    added all component files for video player

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import  React from 'react';
 import ReactDOM from 'react-dom'
+import searchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB0IELbwVklPrhdJPOyUJzfNT_Ei82Xbuk';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <searchBar />
+  </div>
+);
 }
 
 //Take this component's generated HTML and put it on the page(in the DOM)
